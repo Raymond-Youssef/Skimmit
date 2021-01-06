d8ffbf14f3c940c9b4e61774f6f0760e21b2b16b
Raymond Youssef <rimonomega@gmail.com>
2021-01-06 17:02:09 +0200

    Initialized server

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then( (mongoose) => {
+    console.log(`Connected to MongoDB on Port: ${mongoose.connections[0].port}`);
+})
+
+app = express();
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// Routes
+// app.use('/users', require('./routes/users'));
+
+// Start server
+const PORT = process.env.EXPRESS_PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Express is listening on Port: ${PORT}`)
+})
