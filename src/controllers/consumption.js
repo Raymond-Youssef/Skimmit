1d8a9c9cd1603731cb59c00ccc15cecbdc6aeb9a
Raymond Youssef <rimonomega@gmail.com>
2021-01-10 00:49:30 +0200

    better file structure

--- src/controllers/consumption.js
+++ src/controllers/consumption.js
@@ -3,22 +3,6 @@ const {Product} = require('../models/product');
 
 
 module.exports = {
-    consumeProduct: async (req, res, next) => {
-        try {
-            const product = await Product.findByBarcode(req.value.body.barcode);
-            const dailyConsumptionDocument = await Consume.findOrCreateDailyConsumptionDocument(req.user);
-            await dailyConsumptionDocument.consumeProduct(product, req.value.body.quantity);
-            await product.populate('diseases', 'name').execPopulate();
-            res.status(200).json({
-                success: true,
-                data: product,
-            })
-        } catch (err) {
-            next(err);
-        }
-    },
-
-
     todayConsumption: (req, res, next) => {
         Consume.findOrCreateDailyConsumptionDocument(req.user)
             .then( document => {
@@ -44,5 +28,21 @@ module.exports = {
             .catch( (err) => {
                 next(err);
             })
+    },
+
+
+    consumeProduct: async (req, res, next) => {
+        try {
+            const product = await Product.findByBarcode(req.value.body.barcode);
+            const dailyConsumptionDocument = await Consume.findOrCreateDailyConsumptionDocument(req.user);
+            await dailyConsumptionDocument.consumeProduct(product, req.value.body.quantity);
+            await product.populate('diseases', 'name').execPopulate();
+            res.status(200).json({
+                success: true,
+                data: product,
+            })
+        } catch (err) {
+            next(err);
+        }
     },
 }
