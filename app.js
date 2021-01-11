7a7ce24f7a74a54bd3f00cdbab19d8e8b557d0eb
Raymond Youssef <rimonomega@gmail.com>
2021-01-11 03:46:44 +0200

    Added more security measures

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require("helmet");
 const mongoose = require('mongoose');
 require('dotenv').config()
 
@@ -23,6 +25,8 @@ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?
 app = express();
 
 // Middlewares
+app.use(compression());
+app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
